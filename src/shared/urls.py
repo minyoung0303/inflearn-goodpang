cce386541b3dcc1025a9d7335004c3b193371bbb
chorongfire33 <choyeon8317@naver.com>
2025-12-16 21:18:49 +0900

    feat: health check api 생성

--- src/shared/urls.py
+++ src/shared/urls.py
@@ -17,7 +17,17 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+
+base_api = NinjaAPI(title="Goodpang", version="0.0.0")
+
+
+@base_api.get("")
+def health_check_handler(request):
+    return {"ping": "pong"}
+
 
 urlpatterns = [
+    path("", base_api.urls),
     path("admin/", admin.site.urls),
 ]
